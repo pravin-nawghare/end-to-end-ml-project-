5cb69210e9a761244a124f48c746623ac9eef9ab
pravin-nawghare <nawgharepraveen@gmail.com>
2025-02-26 23:06:39 +0530

    hyperparameter added

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,14 +20,21 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(x_train, y_train, x_test, y_test, models):
+def evaluate_model(x_train, y_train, x_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(x_train, y_train)
+            rs = RandomizedSearchCV(model,para,cv=3)
+            rs.fit(x_train,y_train)
+
+            model.set_params(**rs.best_params_)
+            model.fit(x_train,y_train)
+            
+            #model.fit(x_train, y_train)
 
             y_train_pred = model.predict(x_train)
 
